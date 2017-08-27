92e49f119d90915083bc400b913348c1e7d0a96e
Quang-Thuy Hoang <averageAZNguy@gmail.com>
2017-08-26 19:10:21 -0500

    added script to autocollapse menu on mobile after clicking on link

--- script/main.js
+++ script/main.js
@@ -1,2 +1,18 @@
-alert("connected")
-console.log("connected")
+function toggle_drawer() {//toggle collapse menu is-visible
+  var drawer = document.getElementsByClassName('mdl-layout__drawer')[0];
+  var header = document.getElementsByClassName('mdl-layout__header')[0];
+  var obfuscator = document.getElementsByClassName('mdl-layout__obfuscator')[0]
+  drawer.classList.toggle("is-visible");
+  header.classList.toggle("is-casting-shadow");
+  obfuscator.classList.toggle('is-visible');
+  if(drawer.getAttribute('aria-hidden') === false){
+  	drawer.setAttribute('aria-hidden',true)
+  }
+}
+$(document).ready(function () {
+	$('nav a').on("click",function(e){
+		toggle_drawer(); 
+
+	});
+	
+});
--- script/side.js
+++ script/side.js
@@ -1,1 +1,8 @@
-$('.carousel .carousel-slider').carousel({fullWidth: true});
+// alert("connected")
+// $(document).ready(function () {
+//   $(".mdl-layout__drawer nav a").on("click",function(event) {
+//   	console.log("click")
+//     $(".mdl-layout__obfuscator").toggle('is-visible');
+//   });
+//   $('.carousel .carousel-slider').carousel({fullWidth: true});
+// });
